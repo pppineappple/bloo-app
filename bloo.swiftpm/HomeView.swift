4cfe06337946e54188d48bb1e073f15f52927139
Seungah Sn <__@.com>
2025-12-03 17:47:05 -0500

    역사적인 날

--- bloo.swiftpm/HomeView.swift
+++ bloo.swiftpm/HomeView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        NavigationStack{
-            Text("여기에 버튼이 들어갑니다")
+        NavigationStack {
+            VStack {
+                Button {
+                    print("Tapped!")
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color.teal.opacity(0.2))
+                        
+                        VStack {
+                            Image(systemName: "drop.fill")
+                                .foregroundColor(.teal)
+                            
+                            Text("기록하기")
+                                .font(.headline)
+                                .foregroundColor(.teal)
+                        }
+                    }
+                    .aspectRatio(1, contentMode: .fit) // ← 자동 정사각형
+                }
+                .padding()
+            }
+            .navigationTitle("Home")
         }
     }
 }
